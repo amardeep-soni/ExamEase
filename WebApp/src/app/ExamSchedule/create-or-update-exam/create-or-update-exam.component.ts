3623be5e8ab84eb3719d44c5a5fb88295a20fd7c
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2025-01-25 23:29:04 +0545

    Add CreateOrUpdateExam, ExamDetails, and ExamList components with corresponding templates and tests

---
+++ WebApp/src/app/ExamSchedule/create-or-update-exam/create-or-update-exam.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-or-update-exam',
+  imports: [],
+  templateUrl: './create-or-update-exam.component.html',
+  styleUrl: './create-or-update-exam.component.css'
+})
+export class CreateOrUpdateExamComponent {
+
+}
