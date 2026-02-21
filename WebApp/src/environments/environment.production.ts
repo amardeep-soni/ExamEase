5a84cdf941b3992fd1d2a20b5bac88871d8715e5
Amardeep Soni <amardeep10as@gmail.com>
2026-02-21 13:51:04 +0545

    fix: frontend port and environment mapping and appsetting

--- WebApp/src/environments/environment.production.ts
+++ WebApp/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080' // Set to your production backend URL if different
+    apiUrl: 'https://examease-backend-176081228647.us-central1.run.app' // Set to your production backend URL if different
 };
